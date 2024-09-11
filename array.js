21a8e253f9846e5483e3edfa0ee5f7d5afbfae2a
Ahammod <ahammodullah12@gmail.com>
2024-09-11 21:38:34 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,46 @@
+//array//
+var name1, name2, name3, name4, name5;
+name1="Ahammod";
+name2="Josim";
+name3="Sagor";
+name4="Harun";
+name5="Lima";
+
+console.log(name5)
+
+
+
+//array element//
+var names =new Array( );
+names[0]=" Ahammod";
+names[1]=" Afrin*";
+names[2]=" Josim";
+names[3]=" Limon";
+names[4]=" bayzid";
+names[5]=" zara*";
+
+console.log(names[1]);
+console.log(names[5]);
+
+console.log(names);
+ 
+
+
+//array loop//
+var num =[10, 20, 30, 40, 50, 60, 70, 80, 90];
+for( var i=0; i<9; i++){
+    console.log(num[i])
+}
+
+
+//loop array use kore 10ta songkhar moddhe jogfol//
+var num =[10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+let sum=0;
+for( var i=0; i<10; i++){
+    console.log(num[i])
+    sum=sum+num[i];
+}
+console.log("sum= "+sum)
+
+
+
